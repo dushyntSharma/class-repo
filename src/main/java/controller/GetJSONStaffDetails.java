4c7e27e748f50e296476470238b9a7456fd974bd
Shreevatsa MS <shreevatsa.dush@gmail.com>
2021-04-19 16:09:39 +0530

    chnages made

--- src/main/java/controller/GetJSONStaffDetails.java
+++ src/main/java/controller/GetJSONStaffDetails.java
@@ -118,11 +118,11 @@ public class GetJSONStaffDetails extends HttpServlet {
 		}
 
 	}
-/*
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-*/
+
 }
